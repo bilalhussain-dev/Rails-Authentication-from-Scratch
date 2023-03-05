50dd8fd407a2d6c60e9610fe7a8ffe69bbd0df88
Bilal Hussain <bilalhussian785@gmail.com>
2023-03-05 19:24:09 +0500

    adding paginations to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   before_action :set_article_params, only: [:show, :edit, :update, :destroy]
   def index
-    @articles = Article.all
+    @articles = Article.all.page(params[:page])
   end
   def new
     @article = Article.new
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  paginates_per 2
   validates :title, presence: true, length: { minimum: 5, maximum: 1200}
   validates :description, presence: true, length: { minimum: 5, maximum: 1200}
   validates :user_id, presence:true
