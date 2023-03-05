7489ad53bc5fd20a33f1750d73b2831343b4cdca
Bilal Hussain <bilalhussian785@gmail.com>
2023-03-05 16:26:52 +0500

    modified app authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if  @user.update(user_params)
+      flash[:success] = "Hi, #{@user.username}. Settings updated successfully"
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
 
 
   private
